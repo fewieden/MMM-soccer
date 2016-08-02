6c680933a645a95974c6aa28ce50b6e4539b51a3
Ian Perrin <Ian Perrin>
2016-08-02 23:10:57 +0100

    Code comments, loading/no data messages, focus_on and surrounding_team options

--- MMM-soccer.js
+++ MMM-soccer.js
@@ -1,21 +1,36 @@
+/* Magic Mirror
+ * Module: MMM-soccer
+ *
+ * By fewieden https://github.com/fewieden/MMM-soccer
+ *
+ * MIT Licensed.
+ */
+
 Module.register("MMM-soccer",{
+
     defaults: {
         api_key: false,
-        show: 'GERMANY'
+        show: 'GERMANY',
+        focus_on: false,  // false or the name of a team to focus on (used with surrounding_teams)
+        surrounding_teams: false,   // false or the number of teams to show either side of the focused team
+        leagues: {
+            "GERMANY":430,
+            "FRANCE": 434,
+            "ENGLAND": 426,
+            "SPAIN": 436,
+            "ITALY": 438 
+        }
     },
 
+    // A loading boolean.
+    loading: true,
+
+    // Subclass start method.
     start: function() {
         Log.info("Starting module: " + this.name);
-        this.leagues = {
-            'GERMANY': 430,
-            'FRANCE': 434,
-            'ENGLAND': 426,
-            'SPAIN': 436,
-            'ITALY': 401
-        };
-        this.currentLeague = this.leagues[this.config.show];
+        this.currentLeague = this.config.leagues[this.config.show];
         this.getData();
-        setInterval(() => {
+        setInterval(function() {
             this.getData();
         }, this.config.api_key ? 300000 : 1800000); // with api_key every 5min without every 30min
     },
@@ -24,31 +39,56 @@ Module.register("MMM-soccer",{
         this.sendSocketNotification('GET_DATA', {league: this.currentLeague, api_key: this.config.api_key});
     },
 
+    // Subclass socketNotificationReceived method.
     socketNotificationReceived: function(notification, payload){
         if(notification === 'DATA'){
             this.standing = payload;
+            this.loading = (!this.standing);
             this.updateDom();
         }
     },
 
+    // Subclass getStyles method.
     getStyles: function() {
         return ["MMM-soccer.css"];
     },
 
+    // Override dom generator.
     getDom: function() {
         var wrapper = document.createElement("div");
+
+        if (this.loading ||
+            !this.standing) {
+            var title = document.createElement("header");
+            title.innerHTML = this.name;
+            wrapper.appendChild(title);
+
+            var subtitle = document.createElement("div");
+            subtitle.className = "small dimmed light";
+            subtitle.innerHTML = (this.loading) ? this.translate("LOADING") : "No data available" ;
+            wrapper.appendChild(subtitle);
+
+            return wrapper;
+        }
+
+        // Generate Standings Table
         if(this.standing){
-            var title = document.createElement("h4");
+            // League header
+            var title = document.createElement("header");
             title.innerHTML = this.standing.leagueCaption;
             wrapper.appendChild(title);
 
+            // Matchday indicator
             var subtitle = document.createElement("p");
             subtitle.classList.add("xsmall");
             subtitle.innerHTML = "Matchday: " + this.standing.matchday;
             wrapper.appendChild(subtitle);
 
+            // Standings container
             var rows = document.createElement('div');
             rows.classList.add("xsmall");
+
+            // Standings header row
             var row = document.createElement('div');
             row.classList.add('row', 'keys');
 
@@ -62,23 +102,47 @@ Module.register("MMM-soccer",{
 
             var points = document.createElement('img');
             points.classList.add('key-icon');
-            points.src = 'modules/MMM-soccer/icons/points.png';
+            points.src = this.file('icons/points.png');
             details.appendChild(points);
 
             var goals = document.createElement('img');
             goals.classList.add('key-icon');
-            goals.src = 'modules/MMM-soccer/icons/goal.png';
+            goals.src = this.file('icons/goal.png');
             details.appendChild(goals);
             row.appendChild(details);
+
             rows.appendChild(row);
 
-            for(var i = 0; i < this.standing.standing.length; i++){
+            // Get First and Last teams to display in standings
+            var focusTeamIndex;
+            var firstTeam = 0;
+            var lastTeam = this.standing.standing.length;
+            if (this.config.surrounding_teams >= 0 && this.config.focus_on) {
+                for(var i = 0; i < this.standing.standing.length; i++){
+                    if(this.standing.standing[i].teamName === this.config.focus_on){
+                        focusTeamIndex = i;
+                        firstTeam = (focusTeamIndex - this.config.surrounding_teams < 0) ? firstTeam : focusTeamIndex - this.config.surrounding_teams;
+                        lastTeam = (focusTeamIndex + this.config.surrounding_teams + 1 > lastTeam) ? lastTeam : focusTeamIndex + this.config.surrounding_teams + 1;
+                        break;
+                    }
+
+                }
+            }
+
+            // Render Team Rows
+            for(var i = firstTeam; i < lastTeam; i++){
                 var row = document.createElement('div');
                 row.classList.add('row');
 
+                var pos = document.createElement('div');
+                pos.classList.add('position');
+                pos.innerHTML = this.standing.standing[i].position;
+                row.appendChild(pos);
+
                 var icon = document.createElement('img');
                 icon.classList.add('icon');
-                icon.src = this.standing.standing[i].crestURI;
+                if (this.standing.standing[i].crestURI != "null") 
+                    icon.src = this.standing.standing[i].crestURI;   // API returns "null" for teams without a crest
                 row.appendChild(icon);
 
                 var name = document.createElement('div');
@@ -97,11 +161,18 @@ Module.register("MMM-soccer",{
                 goals.innerHTML = this.standing.standing[i].goalDifference;
                 details.appendChild(goals);
                 row.appendChild(details);
+
+                // Create fade in/out effect.
+                if (this.config.surrounding_teams && focusTeamIndex >= 0) {
+                    if (i != focusTeamIndex) {
+                        var currentStep = Math.abs(i - focusTeamIndex);
+                        row.style.opacity = 1 - (1 / this.config.surrounding_teams * currentStep);
+                    }
+                }
+
                 rows.appendChild(row);
             }
             wrapper.appendChild(rows);
-        } else {
-            wrapper.innerHTML = "No data available";
         }
         return wrapper;
     }
