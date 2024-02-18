f37fa9c67fbd985c237324b11e8747a63d00a0b4
Felix Wiedenbach <felix.wiedenbach@fleetster.net>
2024-02-18 20:31:29 +0100

    Update API response mock

--- __mocks__/mockResponse.js
+++ __mocks__/mockResponse.js
@@ -1,64 +1,377 @@
 function generateResponse() {
     return {
-        season: {
-            id: 599,
-            currentMatchday: 14,
-            startDate: '2020-09-18',
-            endDate: '2021-05-15'
+        filters: {
+            season: '2023'
+        },
+        area: {
+            id: 2088,
+            name: 'Germany',
+            code: 'DEU',
+            flag: 'https://crests.football-data.org/759.svg'
         },
         competition: {
             id: 2002,
-            area: {id: 2088, name: 'Germany'},
-            code: 'BL1',
             name: 'Bundesliga',
-            plan: 'TIER_ONE',
-            lastUpdated: '2021-01-03T01:25:08Z'
+            code: 'BL1',
+            type: 'LEAGUE',
+            emblem: 'https://crests.football-data.org/BL1.png'
+        },
+        season: {
+            id: 1592,
+            startDate: '2023-08-18',
+            endDate: '2024-05-18',
+            currentMatchday: 22,
+            winner: null
         },
         standings: [
             {
                 stage: 'REGULAR_SEASON',
+                type: 'TOTAL',
+                group: null,
                 table: [
                     {
                         position: 1,
-                        team: {id: 721, name: 'RB Leipzig', crestUrl: 'https://crests.football-data.org/721.svg'},
-                        playedGames: 14,
-                        points: 31,
-                        won: 9,
+                        team: {
+                            id: 3,
+                            name: 'Bayer 04 Leverkusen',
+                            shortName: 'Leverkusen',
+                            tla: 'B04',
+                            crest: 'https://crests.football-data.org/3.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 18,
                         draw: 4,
-                        lost: 1,
-                        goalsFor: 25,
-                        goalsAgainst: 9,
-                        goalDifference: 16,
-                        form: 'W,D,W,W,D'
+                        lost: 0,
+                        points: 58,
+                        goalsFor: 57,
+                        goalsAgainst: 15,
+                        goalDifference: 42
                     },
                     {
                         position: 2,
-                        team: {id: 5, name: 'FC Bayern München', crestUrl: 'https://crests.football-data.org/5.svg'},
-                        playedGames: 13,
-                        points: 30,
-                        won: 9,
-                        draw: 3,
-                        lost: 1,
-                        goalsFor: 39,
-                        goalsAgainst: 19,
-                        goalDifference: 20,
-                        form: 'W,W,D,D,W'
+                        team: {
+                            id: 5,
+                            name: 'FC Bayern München',
+                            shortName: 'Bayern',
+                            tla: 'FCB',
+                            crest: 'https://crests.football-data.org/5.svg'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 16,
+                        draw: 2,
+                        lost: 4,
+                        points: 50,
+                        goalsFor: 61,
+                        goalsAgainst: 25,
+                        goalDifference: 36
                     },
                     {
                         position: 3,
-                        team: {id: 3, name: 'Bayer 04 Leverkusen', crestUrl: 'https://crests.football-data.org/3.svg'},
-                        playedGames: 14,
-                        points: 28,
-                        won: 8,
+                        team: {
+                            id: 10,
+                            name: 'VfB Stuttgart',
+                            shortName: 'Stuttgart',
+                            tla: 'VFB',
+                            crest: 'https://crests.football-data.org/10.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 15,
+                        draw: 1,
+                        lost: 6,
+                        points: 46,
+                        goalsFor: 51,
+                        goalsAgainst: 28,
+                        goalDifference: 23
+                    },
+                    {
+                        position: 4,
+                        team: {
+                            id: 4,
+                            name: 'Borussia Dortmund',
+                            shortName: 'Dortmund',
+                            tla: 'BVB',
+                            crest: 'https://crests.football-data.org/4.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 11,
+                        draw: 8,
+                        lost: 3,
+                        points: 41,
+                        goalsFor: 44,
+                        goalsAgainst: 27,
+                        goalDifference: 17
+                    },
+                    {
+                        position: 5,
+                        team: {
+                            id: 721,
+                            name: 'RB Leipzig',
+                            shortName: 'RB Leipzig',
+                            tla: 'RBL',
+                            crest: 'https://crests.football-data.org/721.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 12,
                         draw: 4,
-                        lost: 2,
+                        lost: 6,
+                        points: 40,
+                        goalsFor: 48,
+                        goalsAgainst: 28,
+                        goalDifference: 20
+                    },
+                    {
+                        position: 6,
+                        team: {
+                            id: 19,
+                            name: 'Eintracht Frankfurt',
+                            shortName: 'Frankfurt',
+                            tla: 'SGE',
+                            crest: 'https://crests.football-data.org/19.svg'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 8,
+                        draw: 9,
+                        lost: 5,
+                        points: 33,
+                        goalsFor: 34,
+                        goalsAgainst: 28,
+                        goalDifference: 6
+                    },
+                    {
+                        position: 7,
+                        team: {
+                            id: 12,
+                            name: 'SV Werder Bremen',
+                            shortName: 'Bremen',
+                            tla: 'SVW',
+                            crest: 'https://crests.football-data.org/12.svg'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 8,
+                        draw: 5,
+                        lost: 9,
+                        points: 29,
+                        goalsFor: 31,
+                        goalsAgainst: 34,
+                        goalDifference: -3
+                    },
+                    {
+                        position: 8,
+                        team: {
+                            id: 17,
+                            name: 'SC Freiburg',
+                            shortName: 'Freiburg',
+                            tla: 'SCF',
+                            crest: 'https://crests.football-data.org/17.svg'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 8,
+                        draw: 5,
+                        lost: 9,
+                        points: 29,
                         goalsFor: 29,
-                        goalsAgainst: 14,
-                        goalDifference: 15,
-                        form: 'L,L,W,W,W'
+                        goalsAgainst: 40,
+                        goalDifference: -11
+                    },
+                    {
+                        position: 9,
+                        team: {
+                            id: 2,
+                            name: 'TSG 1899 Hoffenheim',
+                            shortName: 'Hoffenheim',
+                            tla: 'TSG',
+                            crest: 'https://crests.football-data.org/2.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 7,
+                        draw: 6,
+                        lost: 9,
+                        points: 27,
+                        goalsFor: 38,
+                        goalsAgainst: 41,
+                        goalDifference: -3
+                    },
+                    {
+                        position: 10,
+                        team: {
+                            id: 44,
+                            name: '1. FC Heidenheim 1846',
+                            shortName: 'Heidenheim',
+                            tla: 'HEI',
+                            crest: 'https://crests.football-data.org/44.svg'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 7,
+                        draw: 6,
+                        lost: 9,
+                        points: 27,
+                        goalsFor: 31,
+                        goalsAgainst: 38,
+                        goalDifference: -7
+                    },
+                    {
+                        position: 11,
+                        team: {
+                            id: 36,
+                            name: 'VfL Bochum 1848',
+                            shortName: 'Bochum',
+                            tla: 'BOC',
+                            crest: 'https://crests.football-data.org/36.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 5,
+                        draw: 10,
+                        lost: 7,
+                        points: 25,
+                        goalsFor: 26,
+                        goalsAgainst: 41,
+                        goalDifference: -15
+                    },
+                    {
+                        position: 12,
+                        team: {
+                            id: 11,
+                            name: 'VfL Wolfsburg',
+                            shortName: 'Wolfsburg',
+                            tla: 'WOB',
+                            crest: 'https://crests.football-data.org/11.svg'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 6,
+                        draw: 6,
+                        lost: 10,
+                        points: 24,
+                        goalsFor: 26,
+                        goalsAgainst: 34,
+                        goalDifference: -8
+                    },
+                    {
+                        position: 13,
+                        team: {
+                            id: 28,
+                            name: '1. FC Union Berlin',
+                            shortName: 'Union Berlin',
+                            tla: 'UNB',
+                            crest: 'https://crests.football-data.org/28.svg'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 7,
+                        draw: 3,
+                        lost: 12,
+                        points: 24,
+                        goalsFor: 21,
+                        goalsAgainst: 35,
+                        goalDifference: -14
+                    },
+                    {
+                        position: 14,
+                        team: {
+                            id: 16,
+                            name: 'FC Augsburg',
+                            shortName: 'Augsburg',
+                            tla: 'FCA',
+                            crest: 'https://crests.football-data.org/16.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 5,
+                        draw: 8,
+                        lost: 9,
+                        points: 23,
+                        goalsFor: 31,
+                        goalsAgainst: 40,
+                        goalDifference: -9
+                    },
+                    {
+                        position: 15,
+                        team: {
+                            id: 18,
+                            name: 'Borussia Mönchengladbach',
+                            shortName: "M'gladbach",
+                            tla: 'BMG',
+                            crest: 'https://crests.football-data.org/18.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 5,
+                        draw: 7,
+                        lost: 10,
+                        points: 22,
+                        goalsFor: 36,
+                        goalsAgainst: 43,
+                        goalDifference: -7
+                    },
+                    {
+                        position: 16,
+                        team: {
+                            id: 1,
+                            name: '1. FC Köln',
+                            shortName: '1. FC Köln',
+                            tla: 'KOE',
+                            crest: 'https://crests.football-data.org/1.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 3,
+                        draw: 7,
+                        lost: 12,
+                        points: 16,
+                        goalsFor: 15,
+                        goalsAgainst: 36,
+                        goalDifference: -21
+                    },
+                    {
+                        position: 17,
+                        team: {
+                            id: 15,
+                            name: '1. FSV Mainz 05',
+                            shortName: 'Mainz',
+                            tla: 'M05',
+                            crest: 'https://crests.football-data.org/15.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 2,
+                        draw: 9,
+                        lost: 11,
+                        points: 15,
+                        goalsFor: 17,
+                        goalsAgainst: 35,
+                        goalDifference: -18
+                    },
+                    {
+                        position: 18,
+                        team: {
+                            id: 55,
+                            name: 'SV Darmstadt 98',
+                            shortName: 'Darmstadt',
+                            tla: 'SVD',
+                            crest: 'https://crests.football-data.org/55.png'
+                        },
+                        playedGames: 22,
+                        form: null,
+                        won: 2,
+                        draw: 6,
+                        lost: 14,
+                        points: 12,
+                        goalsFor: 23,
+                        goalsAgainst: 51,
+                        goalDifference: -28
                     }
-                ],
-                type: 'TOTAL'
+                ]
             }
         ]
     };
